254394b4d1bfa4494da9c25d0d9295f07e29a040
Antek93 <antonio.savino1993@gmail.com>
2023-02-08 18:16:46 +0100

    raggiunta milestone 2

--- src/components/1) AppHeader/Appheader.vue
+++ src/components/1) AppHeader/Appheader.vue
@@ -1,24 +1,51 @@
 <script>
+import SearchForm from './searchForm.vue'
+import axios from 'axios'
+import { store } from '../../store'
+
+
 export default {
-    name: 'AppHeader', 
-    data () {
+    name: "AppHeader",
+    data() {
         return {
-            msg: 'Ciao',
-        }
+            msg: "Ciao",
+            store
+        };
     },
-    created () {
-        
+    created() {
     },
     methods: {
-    }
+        getShow() {
+            axios
+                .get('https://api.themoviedb.org/3/search/movie?api_key=58a47dcf8326b213704d799954c33bd8&query=' + this.store.userInput)
+                .then((response) => {
+                        this.store.userOutcome = response.data.results
+                        console.log(this.store.userOutcome)
+
+                }
+                )
+        }
+    },
+    components: { SearchForm }
 };
 
 </script>
 
 <template>
-
+    <header>
+        <div class="box container-fluid">
+            <div>
+                <h1>Boolfix</h1>
+            </div>
+            <div>
+                <SearchForm @search="getShow" /> 
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss">
-
+.box {
+    height: 10vh;
+}
 </style>
---
+++ src/components/1) AppHeader/searchForm.vue
@@ -0,0 +1,32 @@
+<script>
+import { store } from '../../store'
+export default {
+    name: 'searchForm', 
+    data () {
+        return {
+            variabileNome: '',
+            store
+        }
+    },
+    created () {
+        
+    },
+    methods: {
+    }
+};
+</script>
+
+<template>
+    <div>
+        <form action="#" @submit.prevent="$emit('search')">
+            <input type="text" v-model="store.userInput" placeholder="Search show">
+            <button type="submit">
+            Premi
+             </button>
+        </form>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
